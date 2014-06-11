ceb8d2e94387e93d6acba3b0734d4d2fdba83e4b
Adam Goth <adamgoth@gmail.com>
2014-06-10 21:06:35 -0500

    Rearrange homepage, review format. Remove logic from helper and into PagesController.

--- app/helpers/campsites_helper.rb
+++ app/helpers/campsites_helper.rb
@@ -61,23 +61,4 @@ module CampsitesHelper
     Campsite.uniq.pluck(:state).sort
   end
 
-  def most_recent_reviews
-    Review.find(:all, :order => "id desc", :limit => 5)
-  end
-
-  def highest_rated_campsites
-    Campsite.find_by_sql "SELECT *
-                          FROM (
-                            SELECT distinct c.id
-                              , c.sitename
-                              , round(avg(r.rating), 2) as average  
-                            FROM campsites c
-                              , reviews r
-                            WHERE c.id = r.campsite_id
-                            GROUP BY c.id, c.sitename
-                            )x
-                          ORDER BY x.average DESC
-                          LIMIT 5"
-  end
-
 end
